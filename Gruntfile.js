c53bb8fb1c3efad4a1ba411d168eeee9876ae5dd
francesco-strazzullo <me@francescostrazzullo.info>
2014-09-18 22:25:55 +0200

    creata struttura e Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+// Generated on 2014-09-05 using generator-angular 0.9.7
+'use strict';
+
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// use this if you want to recursively match all subfolders:
+// 'test/spec/**/*.js'
+
+module.exports = function (grunt) {
+
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-ftp-deploy');
+
+  // Define the configuration for all the tasks
+  grunt.initConfig({
+    connect: {
+      server: {
+        options: {
+          port: 9000,
+          keepalive: true,
+          base:'app'
+        }
+      }
+    },
+    'ftp-deploy': {
+        build: {
+          auth: {
+            host: 'ftp.francescostrazzullo.info',
+            port: 21,
+            authKey: 'key1'
+          },
+          src: 'app/',
+          dest: '/www.francescostrazzullo.info/blog/demo/ChromecastApps',
+          exclusions: ['.htaccess']
+        }
+      }
+  });
+
+    
+  grunt.registerTask('serve', 'Compile then start a connect web server', function (target) {
+   
+    grunt.task.run([
+      'connect'
+    ]);
+  });
+
+  grunt.registerTask('deploy', [
+    'ftp-deploy'
+  ]);
+};
